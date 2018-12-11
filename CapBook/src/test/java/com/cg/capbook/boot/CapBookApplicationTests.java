046ba8b87c9c8d3273409dc9ade0eb8de4d9146e
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13110.corp.capgemini.com>
2018-12-11 11:44:19 +0530

    commit by Keshav.

--- CapBook/src/test/java/com/cg/capbook/boot/CapBookApplicationTests.java
+++ CapBook/src/test/java/com/cg/capbook/boot/CapBookApplicationTests.java
@@ -1,10 +1,13 @@
 package com.cg.capbook.boot;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {"features"},
+		glue= {"com.cg.banking.stepdefinition"}
+		)
 public class CapBookApplicationTests {
 	@Test
 	public void contextLoads() {
